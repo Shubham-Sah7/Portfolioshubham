0bc8bd4c09da9866589baeca07bd12664c725e3f
Shubham Sah <shubhamsah@Shubhams-MacBook-Air-3.local>
2026-07-11 18:58:19 +0530

    style: pull desktop deities inward to prevent hand cropping

--- app/components/home/ParallaxImages.tsx
+++ app/components/home/ParallaxImages.tsx
@@ -11,7 +11,7 @@ gsap.registerPlugin(ScrollTrigger)
 const ABHAY = {
   src:    '/images/HomeImages/abhay-v.svg',
   width:  590,
-  left:   '-35%',
+  left:   '-15%',
   top:    '-2%',
   rotate: 35,
 }
@@ -19,7 +19,7 @@ const ABHAY = {
 const TEJAS = {
   src:    '/images/HomeImages/tejas-v.svg',
   width:  510,
-  right:  '-35%',
+  right:  '-15%',
   top:    '40%',
   rotate: -15,
 }
